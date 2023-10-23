14270d746368a31a2f4b260c219ee20bbceb7d48
FrancoSurco <u202015132@upc.edu.pe>
2023-10-22 19:08:59 -0500

    feat(profile-service): added dependencies, mapping configuration adn swagger

---
+++ profile-service/src/main/java/com/digitalholics/profileservice/Profile/resource/Patient/CreatePatientResource.java
@@ -0,0 +1,20 @@
+package com.digitalholics.profileservice.Profile.resource.Patient;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePatientResource {
+
+    private Integer id;
+    private String dni;
+    private Integer age;
+    private String photoUrl;
+    private String birthdayDate;
+    private Integer appointmentQuantity;
+    private String location;
+    private Integer userId;
+}
---
+++ profile-service/src/main/java/com/digitalholics/profileservice/Profile/resource/Physiotherapist/CreatePhysiotherapistResource.java
@@ -0,0 +1,26 @@
+package com.digitalholics.profileservice.Profile.resource.Physiotherapist;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePhysiotherapistResource
+{
+    private Integer id;
+    private String dni;
+    private String specialization;
+    private Integer age;
+    private String location;
+    private String photoUrl;
+    private String birthdayDate;
+    private Double rating;
+    private Integer consultationQuantity;
+    private Integer patientQuantity;
+    private Integer yearsExperience;
+    private Double fees;
+    private Integer userId;
+
+}

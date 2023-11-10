001242296b0477570cf9ece3b48359d9f64619ad
GabrielGomezDlc <89089619+GabrielGomezDlc@users.noreply.github.com>
2023-11-09 20:59:25 -0500

    feat(new-modules):added modulo for therapy-service

---
+++ health-expertise-service/src/main/java/com/digitalholics/healthexpertiseservice/HealthExpertiseServiceApplication.java
@@ -0,0 +1,15 @@
+package com.digitalholics.healthexpertiseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HealthExpertiseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthExpertiseServiceApplication.class, args);
+    }
+
+}

e4a0e00e570c27ed6337ada318db21eef3ff1ad7
GabrielGomezDlc <89089619+GabrielGomezDlc@users.noreply.github.com>
2023-11-19 15:40:03 -0500

    Changes by Maria

--- social-service/src/main/java/com/digitalholics/socialservice/Social/api/rest/ReviewsController.java
+++ social-service/src/main/java/com/digitalholics/socialservice/Social/api/rest/ReviewsController.java
@@ -7,6 +7,13 @@ import com.digitalholics.socialservice.Social.resource.CreateReviewResource;
 import com.digitalholics.socialservice.Social.resource.ReviewResource;
 import com.digitalholics.socialservice.Social.resource.UpdateReviewResource;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/social/reviews", produces = "application/json")
-@Tag(name = "Reviews", description = "Create, read, update and delete reviews   ")
+@Tag(name = "Reviews", description = "Reviews operations: listing, retrieval, registration, update and deletion")
 public class ReviewsController {
     private final ReviewService reviewService;
     private final ReviewMapper mapper;
@@ -26,18 +33,45 @@ public class ReviewsController {
         this.mapper = mapper;
     }
 
+    @Operation(summary = "Get all reviews", description = "Returns reviews' list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully got"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping
     public Page<ReviewResource> getAllReviews(@RequestHeader("Authorization") String jwt, Pageable pageable) {
         return mapper.modelListPage(reviewService.getAll(), pageable);
     }
 
+    @Operation(summary = "Get review by id", description = "Returns review with a provided id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully got"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("{reviewId}")
-    public ReviewResource getReviewById(@RequestHeader("Authorization") String jwt, @PathVariable Integer reviewId) {
+    public ReviewResource getReviewById(
+            @Parameter(hidden = true) @RequestHeader("Authorization") String jwt,
+            @Parameter(description = "Review Id", required = true, examples = @ExampleObject(name = "reviewId", value = "1")) @PathVariable Integer reviewId
+    ) {
         return mapper.toResource(reviewService.getById(reviewId));
     }
 
+    @Operation(summary = "Get reviews by physiotherapist id", description = "Returns reviews with a provided physiotherapist id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully got"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("byPhysiotherapistId/{physiotherapistId}")
-    public Page<ReviewResource> getReviewsByPhysiotherapistId(@RequestHeader("Authorization") String jwt, @PathVariable Integer physiotherapistId, Pageable pageable) {
+    public Page<ReviewResource> getReviewsByPhysiotherapistId(
+            @Parameter(hidden = true)@RequestHeader("Authorization") String jwt,
+            @Parameter(description = "Physiotherapist Id", required = true, examples = @ExampleObject(name = "physiotherapistId", value = "1")) @PathVariable Integer physiotherapistId, Pageable pageable
+    ) {
         return mapper.modelListPage(reviewService.getByPhysiotherapistId(physiotherapistId), pageable);
     }
 

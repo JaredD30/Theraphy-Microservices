5e11526c3b409aca8d40774d921fa1449420fdde
LuisPineda05 <89112808+LuisPineda05@users.noreply.github.com>
2024-05-24 22:24:39 -0500

    feat(theraphy): added diagnosis model

--- therapy-service/src/main/java/com/digitalholics/therapyservice/Therapy/domain/model/entity/External/Diagnosis.java
+++ therapy-service/src/main/java/com/digitalholics/therapyservice/Therapy/domain/model/entity/External/Diagnosis.java
@@ -7,32 +7,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-@Getter
-@Setter
-@With
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "diagnoses")
+@Getter
+@Setter
 public class Diagnosis {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "physiotherapist_id")
-    @JsonIgnore
     private Physiotherapist physiotherapist;
-
-    @ManyToOne
-    @JoinColumn(name = "patient_id")
-    @JsonIgnore
     private Patient patient;
-
     private String diagnosis;
-
-    @NotNull
-    @NotBlank
     private String date;
 }
